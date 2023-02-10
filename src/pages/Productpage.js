6b5b3edc06a00556ba5471474560879bae818fb3
MaxBouricius <55273830+MaxBouricius@users.noreply.github.com>
2023-02-10 18:55:47 +0100

    laatste commit aangezien ik was vergeten het te commiten

--- src/pages/Productpage.js
+++ src/pages/Productpage.js
@@ -1,10 +1,27 @@
 import { useEffect } from "react"
+import { useParams } from "react-router-dom"
 import "./Productpage.css"
 
 const Productpage = (props) => {
     useEffect(() => {
         window.scrollTo(0,0)
     })
+
+    const {type} = useParams()
+    if(type === "wine"){
+        document
+        .getElementsByTagName('meta')
+        .namedItem('description')
+        .setAttribute('content', 'Add wine to card!')
+    }
+
+    if(type === "chicken"){
+        document
+        .getElementsByTagName('meta')
+        .namedItem('description')
+        .setAttribute('content', 'Add chicken to card!')
+    }
+    
     const toBeRendered = props.collection.map(item => {
         return (
             <li key={item.name} className="productpage__listItem">
